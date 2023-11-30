0dd5af42ee941bfd26d0eeeda02c9a9a2b100ac7
2slw4u <mr.fyed@mail.ru>
2023-12-01 00:43:12 +0700

    Constructed the overall project architecture - Note: chosen architecture might change in the future - Created subsuquent folders: --- "layouts" for containing html layouts --- "scripts" for containing js files --- "source" for any files with usages in multiple parts of project ----- "constants" for css and js constants ----- "images" for any images ----- "libraries" for any libraries used in future --- "styles" for containing css files

---
+++ source/libraries/dummy.js
@@ -0,0 +1,3 @@
+/* placeholder for useful libraries
+in the future take a look at https://javascript.plainenglish.io/42-good-javascript-libraries-and-frameworks-for-front-end-developer-5f110148e700
+ */

d25bc91196ee755a6a88d91a3066a86d29a8b54e
2slw4u <mr.fyed@mail.ru>
2023-12-06 16:59:58 +0700

    Added info to libraries - Added info about Underscore.js library should I decide to use it in future

--- source/libraries/dummy.js
+++ source/libraries/dummy.js
@@ -1,10 +1,13 @@
 /* 
 more of them: https://javascript.plainenglish.io/42-good-javascript-libraries-and-frameworks-for-front-end-developer-5f110148e700
 
-
+helps with date-inputs:
 Date-picker: https://github.com/duetds/date-picker#installation
     for using in .html file: 
         <script type="module" src="https://cdn.jsdelivr.net/npm/@duetds/date-picker@1.4.0/dist/duet/duet.esm.js"></script>
         <script nomodule src="https://cdn.jsdelivr.net/npm/@duetds/date-picker@1.4.0/dist/duet/duet.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@duetds/date-picker@1.4.0/dist/duet/themes/default.css" /> 
+
+helps with common programming-problems:
+Underscore.js: https://underscorejs.org/
 */
